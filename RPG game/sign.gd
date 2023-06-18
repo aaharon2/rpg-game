d04f9382229b3cbfd6e5bb18df7f40427f321d7f
aaharon2 <agam.aharon2@gmail.com>
2023-06-18 18:01:07 +1200

    sign code changed (node wasn't correct)

---
+++ RPG game/sign.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+func _sign():
+	pass
+
+	
+func _on_area_2d_body_entered(body):
+	var player = get_node("/root/Game/Player")
+	var player_area = get_node("/root/Game/Player/Detection Area")
+	
+	if body == player_area:
+		player.sign_in_range = true
