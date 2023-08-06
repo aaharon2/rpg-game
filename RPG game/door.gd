b3e4f40e6c8bb11ee3dd9569c8ebcb1daa9f8b87
aaharon2 <agam.aharon2@gmail.com>
2023-08-07 11:10:34 +1200

    door anim

---
+++ RPG game/door.gd
@@ -0,0 +1,25 @@
+extends AnimatableBody2D
+
+var player_in_range = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var anim = $AnimatedSprite2D
+	if player_in_range == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			anim.play()
+			$CollisionShape2D.disabled = true
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		player_in_range = true
+		
+func _on_area_2d_body_exited(body):
+	if body.has_method("player"):
+		player_in_range = false
