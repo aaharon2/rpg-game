f7f9be340cc970f2683a99a96f4877c2d9e0fafb
aaharon2 <agam.aharon2@gmail.com>
2023-08-08 10:53:05 +1200

    removed commented code

--- RPG game/calcaire_town.gd
+++ RPG game/calcaire_town.gd
@@ -15,17 +15,4 @@ func _process(delta):
 func _on_main_map_transition_body_entered(body):
 	if body.has_method("player"):
 		get_tree().change_scene_to_file("res://game.tscn")
-		#Global.world_transition = true
 
-
-#func _on_main_map_transition_body_exited(body):
-	#if body.has_method("player"):
-		#Global.world_transition = false
-		
-#func change_scene():
-	#if Global.world_transition == true:
-		#if Global.cur_scene == "calcaire_town":
-			#get_tree().change_scene_to_file("res://game.tscn")
-			#Global.player_enter_town_posx = 722
-			#Global.player_enter_town_posy = 11
-			#Global.finish_changescenes()
--- RPG game/game.gd
+++ RPG game/game.gd
@@ -9,25 +9,4 @@ func _ready():
 
 func _process(delta):
 	pass
-	#change_scene()
 
-#func _on_town_transition_body_entered(body):
-#	if body.has_method("player"):
-#		_on_level_changed(level_name)
-		#Global.town_transition = true
-
-#func _on_town_transition_body_exited(body):
-	#if body.has_method("player"):
-		#Global.town_transition = false
-		
-#func change_scene():
-	#if Global.town_transition == true:
-		#if Global.current_scene == "world":
-			#get_tree().change_scene_to_file("res://calcaire_town.tscn")
-			#Global.player_exit_town_posx = 720
-			#Global.player_exit_town_posy = 608
-			#Global.finish_changescenes()
-
-
-#func _on_level_changed(level_name):
-#	emit_signal("level_changed", level_name)
